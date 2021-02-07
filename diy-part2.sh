d87eb976910f89a711e4a48838c607d1433d8ca0
Kiss222223 <59243490+Kiss222223@users.noreply.github.com>
2021-02-07 13:26:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/33.33.33.252/g' package/base-files/files/bin/config_generate
+sed -i '/network.$1.netmask/a set network.$1.gateway='%33.33.33.253%'' package/base-files/files/bin/config_generate
+sed -i "s/%/'/g" package/base-files/files/bin/config_generate
